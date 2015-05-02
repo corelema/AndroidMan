b8a936dc5c9f05804f2110d4550db78fd4c3cf8c
corelema <corentin.leman@gmail.com>
2015-05-02 11:32:58 -0500

    Added texture to AndroidMan (need to be optimized).

--- app/src/main/java/com/cocorporation/androidman/GLRenderer.java
+++ app/src/main/java/com/cocorporation/androidman/GLRenderer.java
@@ -13,8 +13,11 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLUtils;
 import android.opengl.Matrix;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -30,6 +33,8 @@ public class GLRenderer implements Renderer {
     // Geometric variables
     public FloatBuffer vertexBuffer;
     public ShortBuffer drawListBuffer;
+    public static float uvs[];
+    public FloatBuffer uvBuffer;
     private static float factor = 1.0f;
     private static float offsetx = 0.0f;
     private static float offsety = 0.0f;
@@ -83,22 +88,70 @@ public class GLRenderer implements Renderer {
         // Get the amount of time the last frame took.
         long elapsed = now - mLastTime;
 
-        // Update our example
-        bufferizeObjects();
+        // Prepare rendering
+        prepareRender();
 
-        // Render our example
-        Render(mtrxProjectionAndView);
+        // Bufferize Background
+        bufferizeBackground();
+
+        int vertexShader = riGraphicTools.loadShader(GLES20.GL_VERTEX_SHADER, riGraphicTools.vs_SolidColor);
+        int fragmentShader = riGraphicTools.loadShader(GLES20.GL_FRAGMENT_SHADER, riGraphicTools.fs_SolidColor);
+
+        riGraphicTools.sp_SolidColor = GLES20.glCreateProgram();             // create empty OpenGL ES Program
+        GLES20.glAttachShader(riGraphicTools.sp_SolidColor, vertexShader);   // add the vertex shader to program
+        GLES20.glAttachShader(riGraphicTools.sp_SolidColor, fragmentShader); // add the fragment shader to program
+        GLES20.glLinkProgram(riGraphicTools.sp_SolidColor);                  // creates OpenGL ES program executables
+
+        // Set our shader programm
+        GLES20.glUseProgram(riGraphicTools.sp_SolidColor);
+
+        // Render Background
+        renderBackground(mtrxProjectionAndView);
+
+        // Bufferize AndroidMan
+        bufferizeAndroidMan();
+
+        // Create the shaders, solid color
+        int vertexShader2 = riGraphicTools.loadShader(GLES20.GL_VERTEX_SHADER,
+                riGraphicTools.vs_SolidColor);
+        int fragmentShader2 = riGraphicTools.loadShader(GLES20.GL_FRAGMENT_SHADER,
+                riGraphicTools.fs_SolidColor);
+
+        riGraphicTools.sp_SolidColor = GLES20.glCreateProgram();
+        GLES20.glAttachShader(riGraphicTools.sp_SolidColor, vertexShader2);
+        GLES20.glAttachShader(riGraphicTools.sp_SolidColor, fragmentShader2);
+        GLES20.glLinkProgram(riGraphicTools.sp_SolidColor);
+
+        // Create the shaders, images
+        vertexShader2 = riGraphicTools.loadShader(GLES20.GL_VERTEX_SHADER,
+                riGraphicTools.vs_Image);
+        fragmentShader2 = riGraphicTools.loadShader(GLES20.GL_FRAGMENT_SHADER,
+                riGraphicTools.fs_Image);
+
+        riGraphicTools.sp_Image = GLES20.glCreateProgram();
+        GLES20.glAttachShader(riGraphicTools.sp_Image, vertexShader2);
+        GLES20.glAttachShader(riGraphicTools.sp_Image, fragmentShader2);
+        GLES20.glLinkProgram(riGraphicTools.sp_Image);
+
+        // Set our shader programm
+        GLES20.glUseProgram(riGraphicTools.sp_Image);
+
+        // Render AndroidMan
+        renderAndroidMan(mtrxProjectionAndView);
 
         // Save the current time to see how long it took <img class="wp-smiley" alt=":)" src="http://androidblog.reindustries.com/wp-includes/images/smilies/icon_smile.gif"> .
         mLastTime = now;
 
     }
 
-    private void Render(float[] m) {
-
+    private void prepareRender()
+    {
         // clear Screen and Depth Buffer, we have set the clear color as black.
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+    }
 
+    private void renderBackground(float[] m)
+    {
         // get handle to vertex shader's vPosition member
         int mPositionHandle = GLES20.glGetAttribLocation(riGraphicTools.sp_SolidColor, "vPosition");
 
@@ -122,7 +175,55 @@ public class GLRenderer implements Renderer {
 
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(mPositionHandle);
+    }
 
+    private void renderAndroidMan(float[] m)
+    {
+        // get handle to vertex shader's vPosition member
+        int mPositionHandle =
+                GLES20.glGetAttribLocation(riGraphicTools.sp_Image, "vPosition");
+
+        // Enable generic vertex attribute array
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        // Prepare the triangle coordinate data
+        GLES20.glVertexAttribPointer(mPositionHandle, 3,
+                GLES20.GL_FLOAT, false,
+                0, vertexBuffer);
+
+        // Get handle to texture coordinates location
+        int mTexCoordLoc = GLES20.glGetAttribLocation(riGraphicTools.sp_Image,
+                "a_texCoord" );
+
+        // Enable generic vertex attribute array
+        GLES20.glEnableVertexAttribArray ( mTexCoordLoc );
+
+        // Prepare the texturecoordinates
+        GLES20.glVertexAttribPointer ( mTexCoordLoc, 2, GLES20.GL_FLOAT,
+                false,
+                0, uvBuffer);
+
+        // Get handle to shape's transformation matrix
+        int mtrxhandle = GLES20.glGetUniformLocation(riGraphicTools.sp_Image,
+                "uMVPMatrix");
+
+        // Apply the projection and view transformation
+        GLES20.glUniformMatrix4fv(mtrxhandle, 1, false, m, 0);
+
+        // Get handle to textures locations
+        int mSamplerLoc = GLES20.glGetUniformLocation (riGraphicTools.sp_Image,
+                "s_texture" );
+
+        // Set the sampler texture unit to 0, where we have saved the texture.
+        GLES20.glUniform1i ( mSamplerLoc, 0);
+
+        // Draw AndroidMan
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, numberOfIndicesToPlot,
+                GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
+        GLES20.glDisableVertexAttribArray(mTexCoordLoc);
     }
 
     @Override
@@ -158,11 +259,15 @@ public class GLRenderer implements Renderer {
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 
         // Fill the buffers
-        bufferizeObjects();
+        bufferizeBackground(); // Is it necessary?
+        bufferizeAndroidMan();
+
+        //Create the AndroidMan information
+        setupImage();
 
         // Set the clear color to black
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1);
-
+/**/
         // Create the shaders
         int vertexShader = riGraphicTools.loadShader(GLES20.GL_VERTEX_SHADER, riGraphicTools.vs_SolidColor);
         int fragmentShader = riGraphicTools.loadShader(GLES20.GL_FRAGMENT_SHADER, riGraphicTools.fs_SolidColor);
@@ -174,33 +279,131 @@ public class GLRenderer implements Renderer {
 
         // Set our shader programm
         GLES20.glUseProgram(riGraphicTools.sp_SolidColor);
+/**/
+/*
+        // Create the shaders, solid color
+        int vertexShader2 = riGraphicTools.loadShader(GLES20.GL_VERTEX_SHADER,
+                riGraphicTools.vs_SolidColor);
+        int fragmentShader2 = riGraphicTools.loadShader(GLES20.GL_FRAGMENT_SHADER,
+                riGraphicTools.fs_SolidColor);
+
+        riGraphicTools.sp_SolidColor = GLES20.glCreateProgram();
+        GLES20.glAttachShader(riGraphicTools.sp_SolidColor, vertexShader2);
+        GLES20.glAttachShader(riGraphicTools.sp_SolidColor, fragmentShader2);
+        GLES20.glLinkProgram(riGraphicTools.sp_SolidColor);
+
+        // Create the shaders, images
+        vertexShader2 = riGraphicTools.loadShader(GLES20.GL_VERTEX_SHADER,
+                riGraphicTools.vs_Image);
+        fragmentShader2 = riGraphicTools.loadShader(GLES20.GL_FRAGMENT_SHADER,
+                riGraphicTools.fs_Image);
+
+        riGraphicTools.sp_Image = GLES20.glCreateProgram();
+        GLES20.glAttachShader(riGraphicTools.sp_Image, vertexShader2);
+        GLES20.glAttachShader(riGraphicTools.sp_Image, fragmentShader2);
+        GLES20.glLinkProgram(riGraphicTools.sp_Image);
+
+        // Set our shader programm
+        GLES20.glUseProgram(riGraphicTools.sp_Image);
+
+        */
     }
 
-    private void bufferizeObjects()
+    public void setupImage()
     {
-        AndroidMan androidMan = entityManagement.getAndroidMan();
+        // Create our UV coordinates.
+        uvs = new float[] {
+                0.0f, 0.0f,
+                0.0f, 1.0f,
+                1.0f, 1.0f,
+                1.0f, 0.0f
+        };
+
+        // The texture buffer
+        ByteBuffer bb = ByteBuffer.allocateDirect(uvs.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        uvBuffer = bb.asFloatBuffer();
+        uvBuffer.put(uvs);
+        uvBuffer.position(0);
+
+        // Generate Textures, if more needed, alter these numbers.
+        int[] texturenames = new int[1];
+        GLES20.glGenTextures(1, texturenames, 0);
+
+        // Retrieve our image from resources.
+        int id = mContext.getResources().getIdentifier("mipmap/ic_launcher", null,
+                mContext.getPackageName());
+
+        // Temporary create a bitmap
+        Bitmap bmp = BitmapFactory.decodeResource(mContext.getResources(), id);
+
+        // Bind texture to texturename
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texturenames[0]);
+
+        // Set filtering
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER,
+                GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER,
+                GLES20.GL_LINEAR);
+
+        // Set wrapping mode
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S,
+                GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T,
+                GLES20.GL_CLAMP_TO_EDGE);
+
+        // Load the bitmap into the bound texture.
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bmp, 0);
+
+        // We are done using the bitmap so we should recycle it.
+        bmp.recycle();
+
+    }
+
+    private void bufferizeBackground()
+    {
 
         // The vertex buffer.
         float[] backgroundVertices = background.getVertices();
-        float[] androidManVertices = androidMan.generateVertices();
         //to do:ghosts
-        ByteBuffer bb = ByteBuffer.allocateDirect((backgroundVertices.length + androidManVertices.length) * 4);
+        ByteBuffer bb = ByteBuffer.allocateDirect((backgroundVertices.length * 4));
         bb.order(ByteOrder.nativeOrder());
         vertexBuffer = bb.asFloatBuffer();
         vertexBuffer.put(backgroundVertices);
-        vertexBuffer.put(androidManVertices);
         vertexBuffer.position(0);
 
         // initialize byte buffer for the draw list
         short[] backgroundIndices = background.getIndices();
-        short[] androidManIndices = androidMan.getIndices();
-        addOffsetToIndies(androidManIndices, (short)(backgroundIndices.length*4/6));
-        numberOfIndicesToPlot = backgroundIndices.length + androidManIndices.length;
+        numberOfIndicesToPlot = backgroundIndices.length;
 
         ByteBuffer dlb = ByteBuffer.allocateDirect(numberOfIndicesToPlot* 2);
         dlb.order(ByteOrder.nativeOrder());
         drawListBuffer = dlb.asShortBuffer();
         drawListBuffer.put(backgroundIndices);
+        drawListBuffer.position(0);
+    }
+
+    private void bufferizeAndroidMan()
+    {
+        AndroidMan androidMan = entityManagement.getAndroidMan();
+
+        // The vertex buffer.
+        float[] androidManVertices = androidMan.generateVertices();
+        //to do:ghosts
+        ByteBuffer bb = ByteBuffer.allocateDirect((androidManVertices.length) * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(androidManVertices);
+        vertexBuffer.position(0);
+
+        // initialize byte buffer for the draw list
+        short[] androidManIndices = androidMan.getIndices();
+        numberOfIndicesToPlot = androidManIndices.length;
+
+        ByteBuffer dlb = ByteBuffer.allocateDirect(numberOfIndicesToPlot* 2);
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
         drawListBuffer.put(androidManIndices);
         drawListBuffer.position(0);
     }
--- app/src/main/java/com/cocorporation/androidman/riGraphicTools.java
+++ app/src/main/java/com/cocorporation/androidman/riGraphicTools.java
@@ -9,6 +9,7 @@ public class riGraphicTools {
 
     // Program variables
     public static int sp_SolidColor;
+    public static int sp_Image;
 
     /* SHADER Solid
      *
@@ -26,6 +27,29 @@ public class riGraphicTools {
             "precision mediump float;" +
                     "void main() {" +
                     "  gl_FragColor = vec4(0.5,0,0,1);" +
+                    "}";
+
+    /* SHADER Image
+    *
+    * This shader is for rendering 2D images straight from a texture
+    * No additional effects.
+    *
+    */
+    public static final String vs_Image =
+            "uniform mat4 uMVPMatrix;" +
+                    "attribute vec4 vPosition;" +
+                    "attribute vec2 a_texCoord;" +
+                    "varying vec2 v_texCoord;" +
+                    "void main() {" +
+                    "  gl_Position = uMVPMatrix * vPosition;" +
+                    "  v_texCoord = a_texCoord;" +
+                    "}";
+    public static final String fs_Image =
+            "precision mediump float;" +
+                    "varying vec2 v_texCoord;" +
+                    "uniform sampler2D s_texture;" +
+                    "void main() {" +
+                    "  gl_FragColor = texture2D( s_texture, v_texCoord );" +
                     "}";
 
     public static int loadShader(int type, String shaderCode){
